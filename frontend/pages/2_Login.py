e9986b303b8e1dbdda97533c1fca45b279a9235c
ha6374 <hp929456@gmail.com>
2025-12-13 20:02:35 +0530

    Update 2_Login.py

--- frontend/pages/2_Login.py
+++ frontend/pages/2_Login.py
@@ -12,16 +12,19 @@ password = st.text_input("Password", type="password")
 if st.button("Login"):
     response = login(email, password)
 
-    if response.status_code == 200:
+    if response is None:
+        st.error("❌ Server not responding. Please try again later.")
+
+    elif response.status_code == 200:
         st.session_state.token = response.json().get("token")
         st.success("Login Successful!")
-        st.switch_page("5_Dashboard")
+        st.switch_page("pages/5_Dashboard")
     else:
         st.error("Invalid credentials")
 
 if st.button("Forgot Password?"):
-    st.switch_page("4_Forgot_Password")
+    st.switch_page("pages/4_Forgot_Password")
 
 st.write("Don't have an account?")
 if st.button("Create Account"):
-    st.switch_page("3_Signup")
+    st.switch_page("pages/3_Signup")
